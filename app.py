23e96048c50b3729b50f7a718883028f23528b78
249x5a02c7-cmd <249x5a02c7@gprec.ac.in>
2026-01-06 14:48:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,5 +159,9 @@ def admin_logout():
     return redirect("/admin")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
